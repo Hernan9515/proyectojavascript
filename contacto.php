e096162721517851dfd95d9b7bcd6e764fdb9016
Hernan9515 <hdem_1995@hotmail.com>
2022-07-01 21:50:26 -0500

    actualizacion

--- contacto.php
+++ contacto.php
@@ -1,22 +1,4 @@
 <?php include "head.php" ?>
-<header class="hero">
-    <nav class="nav__hero">
-      <div class="container nav__container">
-        <div class="logo">
-          <a href="index.html"><img src="img/logotipo.png" alt="logotipo" width="370px" height="60px"></a>
-        </div>
-        <div class="links">
-          <a href="#" class="link">Inicio</a>
-          <a href="#" class="link">Servicios</a>
-          <a href="#" class="link">Proyectos</a>
-          <a href="#" class="link">Clientes</a>
-          <a href="#" class="link">Capacitaciones</a>
-          <a href="index.html" class="link link--active">Contacto</a>
-        </div>
-      </div>
-    </nav>
-  </header>
-
   <form class="form-register" onsubmit="salida()" id="form"> 
     <h4>Formulario Registro</h4>
     <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
